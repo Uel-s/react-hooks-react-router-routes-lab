9289e6d9de795fc4f3a60598c208b9ae388c40db
samuel <samuelkariuki1@student.moringaschool.com>
2023-07-26 15:08:45 +0300

    route lab

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,23 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 function Home() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <ul>
+        <li>
+          <NavLink to="/movies">Movies Page</NavLink>
+        </li>
+        <li>
+          <NavLink to="/directors">Directors Page</NavLink>
+        </li>
+        <li>
+          <NavLink to="/actors">Actors Page</NavLink>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
 export default Home;
